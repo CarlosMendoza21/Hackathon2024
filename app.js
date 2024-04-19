df4b482474a1d1649db468f416c5fc4a7d4a862b
SebasChips <sebastianalarconbejar@gmail.com>
2024-04-19 15:33:34 -0600

    bootstrapBetter

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'static', 'main.html'));
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static', 'main.html'));
+});
+
 const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
