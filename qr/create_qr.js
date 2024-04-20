b57496a3ef219f104f325672bb4c4d78e02e4322
BraulioGuevara <guevarabraulio8@gmail.com>
2024-04-20 05:20:05 -0600

    create qr updated

--- qr/create_qr.js
+++ qr/create_qr.js
@@ -1,8 +1,10 @@
-const QRCode = require('qrcode');
+function createQR(tipoPeso,tamano){
+  const QRCode = require('qrcode');
 
-QRCode.toFile('file.png', 'Encode this text in QR code', {
-  errorCorrectionLevel: 'H'
-}, function(err) {
-  if (err) throw err;
-  console.log('QR code saved!');
-});
+  QRCode.toFile('file.png', "{tipo: "+tipoPeso+", tamano:{ ancho_maximo : "+tamano[0]+", largo_maximo: tamano[1]} }}", {
+    errorCorrectionLevel: 'H'
+  }, function(err) {
+    if (err) throw err;
+    console.log('QR code saved!');
+  });
+}
